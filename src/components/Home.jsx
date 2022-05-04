5134e35323b0d562bf35e813e1023b14250c564e
nipengg <nevillecorneliustj@gmail.com>
2022-05-04 15:52:17 +0700

    add react scroll for page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
+import { Link } from 'react-scroll';
 
 const Home = () => {
     return (
-        <div className="w-full h-screen bg-[#0A192F]">
+        <div name="home" className="w-full h-screen bg-[#0A192F]">
             <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
                 <p className="text-pink-600">Hi,</p>
                 <h1 className="text-4xl sm:text-7xl font-bold text-[#CCD6F6]">I'm Neville Cornelius Tjampan 🦆</h1>
@@ -11,7 +12,9 @@ const Home = () => {
                 <p className="text-[#8892b0] py-4 max-w-[700px]">I'm a software engineer that love exploring around web development stuff especially javascript</p>
                 <div>
                     <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 duration-300">
-                        Checkout my work
+                        <Link to="work" smooth={true} duration={500}>
+                            Checkout my work
+                        </Link>
                         <span className="group-hover:rotate-90 duration-300">
                             <HiArrowNarrowRight className="ml-3" />
                         </span>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import Logo from '../assets/logo.png';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -12,17 +13,37 @@ const Navbar = () => {
     }
 
     return (
-        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0A192F] text-gray-300">
+        <div name="navbar" className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0A192F] text-gray-300">
             <div>
                 <img src={Logo} alt="Logo Image" style={{ width: '50px' }} />
             </div>
 
             <ul className="hidden md:flex">
-                <li>Home</li>
-                <li>About</li>
-                <li>Tech</li>
-                <li>Work</li>
-                <li>Contact</li>
+                <li>
+                    <Link to="home" smooth={true} duration={500}>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="about" smooth={true} duration={500}>
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link to="skills" smooth={true} duration={500}>
+                        Tech
+                    </Link>
+                </li>
+                <li>
+                    <Link to="work" smooth={true} duration={500}>
+                        Work
+                    </Link>
+                </li>
+                <li>
+                    <Link to="contact" smooth={true} duration={500}>
+                        Contact
+                    </Link>
+                </li>
             </ul>
 
             <div onClick={handleClick} className="md:hidden z-10">
@@ -30,11 +51,30 @@ const Navbar = () => {
             </div>
 
             <ul className={nav === true ? "absolute top-0 left-0 w-full h-screen bg-[#0A192F] flex flex-col justify-center items-center" : "hidden"}>
-                <li className="py-6 text-4xl">Home</li>
-                <li className="py-6 text-4xl">About</li>
-                <li className="py-6 text-4xl">Skills</li>
-                <li className="py-6 text-4xl">Work</li>
-                <li className="py-6 text-4xl">Contact</li>
+                <li className="py-6 text-4xl">
+                    <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+                        Home
+                    </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                    <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+                        About
+                    </Link></li>
+                <li className="py-6 text-4xl">
+                    <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+                        Tech
+                    </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                    <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+                        Work
+                    </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                    <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+                        Contact
+                    </Link>
+                </li>
             </ul>
 
             <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
